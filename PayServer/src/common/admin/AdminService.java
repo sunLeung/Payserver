fdbd3cad79a15f01e8168716825c3b44762a6acc
liangyx <liangyuxin.02@gmail.com>
2014-07-03 22:21:23 +0800

    Common development.

--- PayServer/src/common/admin/AdminService.java
+++ PayServer/src/common/admin/AdminService.java
@@ -4,12 +4,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import utils.Response;
-
 import common.config.AppContent;
+import common.config.UnionsContent;
 
 public class AdminService {
 	
 	public static void getAppsInfo(HttpServletRequest res,HttpServletResponse resp){
 		Response.jsonResp(resp, AppContent.appContent,"application/json;charset=UTF-8");
+	}
+	
+	public static void getUnionsInfo(HttpServletRequest res,HttpServletResponse resp){
+		Response.jsonResp(resp, UnionsContent.unionsContent,"application/json;charset=UTF-8");
 	}
 }
