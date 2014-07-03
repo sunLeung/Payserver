8fdc6f392ac0c892ccd42d225387cbc8f09fc6bb
liangyx <liangyuxin.02@gmail.com>
2014-07-03 22:22:16 +0800

    Common development.

--- PayServer/WebRoot/scripts/controllers.js
+++ PayServer/WebRoot/scripts/controllers.js
@@ -2,7 +2,15 @@ app.controller('getAppsInfo', ['$scope','service', function($scope,service) {
 	service.getApps().success(function(data, status, headers, config) {
 		$scope.apps = data;
   	}).error(function(data, status, headers, config) {
-  		cosole.log('error status:'+status+'  data:'+data);
+  		cosole.log('error getAppsInfo status:'+status+'  data:'+data);
+	});
+}]);
+
+app.controller('getUnionsInfo', ['$scope','service', function($scope,service) {
+	service.getUnions().success(function(data, status, headers, config) {
+		$scope.unions = data;
+  	}).error(function(data, status, headers, config) {
+  		cosole.log('error getUnionsInfo status:'+status+'  data:'+data);
 	});
 }]);
 
